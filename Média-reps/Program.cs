d775222d41e71ac8c0fc3330d2da063f4c9f2865
Susigan <susigan@gmail.com>
2020-08-18 12:29:55 -0300

    versão com writeLine

---
+++ Média-reps/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Média_reps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite o numero de series");
+            int n = int.Parse(Console.ReadLine());
+
+            double[] vect = new double[n];
+
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Reps da Série #" + i );
+                vect[i] = double.Parse(Console.ReadLine());
+
+            }
+
+            double sum = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += vect[i];
+            }
+
+            double avg = sum / n;
+
+            Console.WriteLine(" a média " + avg.ToString("F2"));
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Média-reps/point.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Média_reps
+{
+    struct point
+    {
+        public double X;
+        public double Y;
+
+        public override string ToString()
+        {
+            return "(" + X + ", " + Y + ")";
+        }
+    }
+}
